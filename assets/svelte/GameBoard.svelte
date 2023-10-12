c97d6be8d5595e7d3425e025f90c24e352959012
rekkice <lekkice0@gmail.com>
2023-10-12 15:18:44 -0300

    fixed modals style on mobile

--- assets/svelte/GameBoard.svelte
+++ assets/svelte/GameBoard.svelte
@@ -77,10 +77,10 @@
 
 {#if match.game_status == "waiting" && isHost}
 <div class="flex items-center justify-center fixed left-0 bottom-0 w-full h-full bg-gray-800/20 text-seagull-800">
-    <div class="bg-white rounded-2xl min-w-[500px] py-2 shadow-lg">
+    <div class="bg-white rounded-2xl sm:min-w-[500px] mx-2 text-center py-2 shadow-lg">
       <div class="flex flex-col items-start p-4">
         <div class="flex flex-col items-center gap-4 mx-auto">
-          <div class="text-seagull-900 font-medium text-2xl">Waiting for another player to join</div>
+          <div class="text-seagull-900 font-medium text-xl sm:text-2xl">Waiting for another player to join</div>
           <svg class="animate-spin ml-1 mr-3 h-10 w-10 text-seagull-700" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
             <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
             <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
@@ -107,7 +107,7 @@
 
 {#if match.game_status == "waiting" && !isHost}
 <div class="flex items-center justify-center fixed left-0 bottom-0 w-full h-full bg-gray-800/20 text-seagull-800">
-    <div class="bg-white rounded-2xl min-w-[500px] py-2 shadow-lg">
+    <div class="bg-white rounded-2xl w-full sm:min-w-[500px] sm:w-auto mx-2 py-2 shadow-lg">
       <div class="flex flex-col items-start p-4">
         <div class="flex flex-col items-center gap-4 mx-auto">
           <div class="text-seagull-900 font-medium text-2xl">Hello!</div>
