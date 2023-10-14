a243253188b94e6e56b6ef9f3a8c8f53931d43a8
rekkice <lekkice0@gmail.com>
2023-10-14 10:46:25 -0300

    formatting

--- assets/svelte/GameBoard.svelte
+++ assets/svelte/GameBoard.svelte
@@ -7,8 +7,10 @@
     $: console.log("Match state: ", match)
 
     const hostSymbol = "X"
-    
-    const inviteURL = match.short_link_id ? getInviteURL(match.short_link_id.code) : "null"
+
+    const inviteURL = match.short_link_id
+        ? getInviteURL(match.short_link_id.code)
+        : "null"
 
     function getInviteURL(code) {
         if (typeof window == "undefined") return "Loading..."
@@ -17,7 +19,8 @@
     }
 
     let heading = `${getPlayerTurn(match)}'s turn`
-    $: if (match.game_status == "ongoing") heading = `${getPlayerTurn(match)}'s turn`
+    $: if (match.game_status == "ongoing")
+        heading = `${getPlayerTurn(match)}'s turn`
     function getPlayerTurn(match) {
         if (match.turn == hostSymbol) return match.host_player
         else return match.guest_player
@@ -25,10 +28,11 @@
 
     let nickname: string
 
-    $: if (match.game_status != "waiting_join" && live) statusNotify(match.game_status)
+    $: if (match.game_status != "waiting_join" && live)
+        statusNotify(match.game_status)
 
     function statusNotify(status) {
-        switch(status) {
+        switch (status) {
             case "X_won": {
                 heading = `${match.host_player} won!`
                 break
@@ -46,24 +50,31 @@
     }
 
     function makeMove(i: number) {
-        live.pushEvent("make_move", {id: match.id, index: i, symbol: playerSymbol})
+        live.pushEvent("make_move", {
+            id: match.id,
+            index: i,
+            symbol: playerSymbol,
+        })
     }
 
     function copyToClipboard() {
         navigator.clipboard.writeText(inviteURL)
-        live.pushEvent("put_flash", {type: "info", message: "Copied to clipboard!"})
+        live.pushEvent("put_flash", {
+            type: "info",
+            message: "Copied to clipboard!",
+        })
     }
 
     function shareInvite() {
         navigator.share({
             title: "tic tac toe thing",
             text: "play tictactoe with me!",
-            url: inviteURL
+            url: inviteURL,
         })
     }
 
     function nicknameInput() {
-        live.pushEvent("guest_nickname_input", {id: match.id, nickname})
+        live.pushEvent("guest_nickname_input", { id: match.id, nickname })
     }
 </script>
 
@@ -71,10 +82,16 @@
 
 <h1 class="text-4xl text-center mb-2 text-seagull-800">{heading}</h1>
 
-<div class="w-full h-full grid grid-cols-3 grid-rows-3 relative after:absolute after:w-full after:h-full after:bg-transparent after:border-4 after:border-seagull-200 after:pointer-events-none">
+<div
+    class="w-full h-full grid grid-cols-3 grid-rows-3 relative after:absolute after:w-full after:h-full after:bg-transparent after:border-4 after:border-seagull-200 after:pointer-events-none"
+>
     {#each match.state as cell, i}
         <button
-            disabled={(cell == "X" || cell == "O") || (match.turn != playerSymbol) || (match.game_status != "ongoing") || (role == "spectator")}
+            disabled={cell == "X" ||
+                cell == "O" ||
+                match.turn != playerSymbol ||
+                match.game_status != "ongoing" ||
+                role == "spectator"}
             class="h-full w-full text-[600%] select-none flex items-center justify-center aspect-square border-4 rounded-lg border-seagull-900 text-seagull-900"
             on:click={() => makeMove(i)}
         >
@@ -84,57 +101,109 @@
 </div>
 
 {#if match.game_status == "waiting_join" && role == "host"}
-<div class="flex items-center justify-center fixed left-0 bottom-0 w-full h-full bg-gray-800/20 text-seagull-800">
-    <div class="bg-white rounded-2xl sm:min-w-[500px] mx-2 text-center py-2 shadow-lg">
-      <div class="flex flex-col items-start p-4">
-        <div class="flex flex-col items-center gap-4 mx-auto">
-          <div class="text-seagull-900 font-medium text-xl sm:text-2xl">Waiting for another player to join</div>
-          <svg class="animate-spin ml-1 mr-3 h-10 w-10 text-seagull-700" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-          </svg>
-        </div>
+    <div
+        class="flex items-center justify-center fixed left-0 bottom-0 w-full h-full bg-gray-800/20 text-seagull-800"
+    >
+        <div
+            class="bg-white rounded-2xl sm:min-w-[500px] mx-2 text-center py-2 shadow-lg"
+        >
+            <div class="flex flex-col items-start p-4">
+                <div class="flex flex-col items-center gap-4 mx-auto">
+                    <div
+                        class="text-seagull-900 font-medium text-xl sm:text-2xl"
+                    >
+                        Waiting for another player to join
+                    </div>
+                    <svg
+                        class="animate-spin ml-1 mr-3 h-10 w-10 text-seagull-700"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                    >
+                        <circle
+                            class="opacity-25"
+                            cx="12"
+                            cy="12"
+                            r="10"
+                            stroke="currentColor"
+                            stroke-width="4"
+                        />
+                        <path
+                            class="opacity-75"
+                            fill="currentColor"
+                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                        />
+                    </svg>
+                </div>
 
-        <label class="flex flex-col mx-auto my-4 font-semibold w-5/6">
-            Invite link
-            <input type="text" value={inviteURL} class="font-normal" readonly>
-        </label>
+                <label class="flex flex-col mx-auto my-4 font-semibold w-5/6">
+                    Invite link
+                    <input
+                        type="text"
+                        value={inviteURL}
+                        class="font-normal"
+                        readonly
+                    />
+                </label>
 
-        <div class="mx-auto">
-          <button on:click={shareInvite} class="mx-1 bg-seagull-600 hover:bg-seagull-700 text-white font-bold py-2 px-4 rounded">
-            Share
-          </button>
-          <button on:click={copyToClipboard} class="mx-1 bg-transparent hover:bg-gray-200 text-seagull-700 font-semibold py-2 px-4 border border-seagull-600 rounded">
-            Copy to clipboard
-          </button>
+                <div class="mx-auto">
+                    <button
+                        on:click={shareInvite}
+                        class="mx-1 bg-seagull-600 hover:bg-seagull-700 text-white font-bold py-2 px-4 rounded"
+                    >
+                        Share
+                    </button>
+                    <button
+                        on:click={copyToClipboard}
+                        class="mx-1 bg-transparent hover:bg-gray-200 text-seagull-700 font-semibold py-2 px-4 border border-seagull-600 rounded"
+                    >
+                        Copy to clipboard
+                    </button>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 {/if}
 
 {#if match.game_status == "waiting_join" && role == "guest"}
-<div class="flex items-center justify-center fixed left-0 bottom-0 w-full h-full bg-gray-800/20 text-seagull-800">
-    <div class="bg-white rounded-2xl w-full sm:min-w-[500px] sm:w-auto mx-2 py-2 shadow-lg">
-      <div class="flex flex-col items-start p-4">
-        <div class="flex flex-col items-center gap-4 mx-auto">
-          <div class="text-seagull-900 font-medium text-2xl">Hello!</div>
-        </div>
+    <div
+        class="flex items-center justify-center fixed left-0 bottom-0 w-full h-full bg-gray-800/20 text-seagull-800"
+    >
+        <div
+            class="bg-white rounded-2xl w-full sm:min-w-[500px] sm:w-auto mx-2 py-2 shadow-lg"
+        >
+            <div class="flex flex-col items-start p-4">
+                <div class="flex flex-col items-center gap-4 mx-auto">
+                    <div class="text-seagull-900 font-medium text-2xl">
+                        Hello!
+                    </div>
+                </div>
 
-        <form class="w-full" on:submit|preventDefault={nicknameInput}>
-            <label class="flex flex-col mx-auto my-4 font-semibold w-5/6">
-                Enter a nickname to start
-                <input type="text" placeholder="Nickname" minlength="3"  maxlength="25" required class="font-normal invalid:ring-red-500" bind:value={nickname}>
-            </label>
+                <form class="w-full" on:submit|preventDefault={nicknameInput}>
+                    <label
+                        class="flex flex-col mx-auto my-4 font-semibold w-5/6"
+                    >
+                        Enter a nickname to start
+                        <input
+                            type="text"
+                            placeholder="Nickname"
+                            minlength="3"
+                            maxlength="25"
+                            required
+                            class="font-normal invalid:ring-red-500"
+                            bind:value={nickname}
+                        />
+                    </label>
 
-            <div class="w-full flex justify-center">
-                <button class="bg-seagull-600 hover:bg-seagull-700 text-white font-bold py-2 px-4 rounded">
-                    Done
-                </button>
+                    <div class="w-full flex justify-center">
+                        <button
+                            class="bg-seagull-600 hover:bg-seagull-700 text-white font-bold py-2 px-4 rounded"
+                        >
+                            Done
+                        </button>
+                    </div>
+                </form>
             </div>
-        </form>
-
-      </div>
+        </div>
     </div>
-  </div>
 {/if}
